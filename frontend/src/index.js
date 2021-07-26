521bbd4ce3fa29f9664cb08f3f46e38eb6c29615
Wilson Koh <NikoKoh98@gmail.com>
2021-07-26 15:17:05 +0800

    Added user auth services & basic router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter
+} from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
---
+++ frontend/src/services/Auth.js
@@ -0,0 +1,43 @@
+import AxiosInstance from './AxiosFactory';
+import LocalStorageUtil from '../utils/LocalStorage';
+
+export default class Auth {
+    constructor() {
+        this.token = LocalStorageUtil.read("token");
+    }
+
+    async login(payload) {
+        try {
+            const response = await AxiosInstance.post("/login/", payload, {
+                headers: {
+                    "Authorization": `Bearer ${this.token}`
+                }
+            });
+            return response;
+        } catch(err) {
+            console.log(err);
+        }
+    }
+
+    async register(payload) {
+        try {
+            const response = await AxiosInstance.post("/register/", payload, {
+                headers: {
+                    "Authorization": `Bearer ${this.token}`
+                }
+            });
+            return response;
+        } catch(err) {
+            console.log(err);
+        }
+    }
+
+    async logout() {
+        try {
+            LocalStorageUtil.remove("token");
+            return true;
+        } catch(err) {
+            return false;
+        }
+    }
+}
