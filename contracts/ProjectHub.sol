bbc5375acc4c3c0c949adf3efe8ab74a7173a3d0
ryanSacatani <sacataniryan1@gmail.com>
2021-08-08 20:38:41 +0800

    Combined project and projectHub contract in 1 contract

--- contracts/ProjectHub.sol
+++ contracts/ProjectHub.sol
@@ -1,8 +1,119 @@
 // SPDX-License-Identifier: GPL-3.0
-
 pragma solidity >=0.4.22 <0.9.0;
 
-import "./Project.sol";
+contract Project{
+    // Storage state cariables
+    address public owner;
+    string public description;
+    uint public minDonation;
+    uint public raisedDonation = 0;
+    uint public goal;
+    uint public numberOfDonors;
+    mapping (address => uint) public donations; // stores the donation of each donor
+    uint numRequests;
+    mapping (uint => Request) requests;
+    
+    // For each spending request, need to create a new instance of request
+    struct Request {
+        string requestDescription;
+        uint value;
+        address payable recipient; // This is the address that the beneificiary want to send funds to.
+        bool completed;
+        uint numberOfVoters;
+        mapping (address=>bool) voters;
+    }
+    
+    // Events needed by the web app
+    event Contribute( address donor, uint amount);
+    event CreateRequest( address owner, address recipient, string requestDescription, uint value );
+    event PaymentMade( address owner, address recipient, uint value );
+    event DonorVote( address donor);
+    
+    // Modifiers to automatically check a condition, prior to executing a function
+    modifier onlyAdmin{
+        require(msg.sender == owner);
+        _; // This is just syntax, this tells the modifier to continue with the rest of the function after it calls its body
+    }
+    
+    modifier goalReached {
+        // Allow owner to create spending requrest only when the goal is reached
+        require(raisedDonation >= goal);
+        _;
+    }
+    
+    constructor( string memory _description, uint _minDonation, uint _goal) public {
+        description = _description;
+        minDonation = _minDonation;
+        goal = _goal;
+        owner = msg.sender; // The creator of the project is the owner of the project
+        numRequests = 0;
+    }
+    
+    function contribute() public payable{
+        // A payable function will store the value that is sent to that account
+        // and will be trapped there unless there is a withdraw function
+        // Conditions for executing the rest of the function
+        require( msg.value > minDonation);
+        // Checking whether this is the first time the person is contributing
+        if(donations[msg.sender] == 0){
+            // If its his first time, need to invrement the total number of contributors
+            numberOfDonors ++;
+        }
+        // Add the amount sent to his previous contributions
+        donations[msg.sender] += msg.value;
+        raisedDonation += msg.value;
+        emit Contribute( msg.sender, msg.value);
+    }
+    
+    // Project owner creates a request before he can spend his money
+    function create_request( string memory _requestDescription, address payable _recipient, uint _value) public onlyAdmin goalReached{
+        // instantiating a request struct in memory
+        // Store request struct variable from mapping in storage
+        Request storage newRequest = requests[numRequests++];
+        newRequest.requestDescription = _requestDescription;
+        newRequest.value = _value;
+        newRequest.recipient = _recipient;
+        newRequest.numberOfVoters = 0;
+        newRequest.completed = false;
+        emit CreateRequest( msg.sender, _recipient, _requestDescription, _value);
+    }
+    
+    function vote_request(uint index) public goalReached{
+        
+        // Direct Reference a specific spending request inside the requests dynamic array
+        // by calling storage here,, any changes that we make to thisRequest will
+        // also affect the request in tthe specific index in requests array
+        Request storage thisRequest =  requests[index];
+        // Checking for Conditions
+        // The voter also needs to be a donor
+        require(donations[msg.sender] > 0);
+        // The voter cannot double vote
+        require(thisRequest.voters[msg.sender] == false);
+        // Counting the vote 
+        // We only count positive votes
+        thisRequest.voters[msg.sender] = true;
+        thisRequest.numberOfVoters++;
+        emit DonorVote(msg.sender);
+    }
+    
+    
+    function make_payment( uint index) public onlyAdmin goalReached{
+        // Directly reference the specfic spending request inside the request dynamic array
+        Request storage thisRequest = requests[index];
+        
+        // Check necessary Conditions 
+        // Cannot allow double payment for the same request
+        require(thisRequest.completed == false);
+        // Number of voters that approved should be more thatn 50% voters
+        require(thisRequest.numberOfVoters > numberOfDonors /2);
+        
+        // Performing the transder operation
+        thisRequest.recipient.transfer(thisRequest.value);
+        thisRequest.completed = true;
+        emit PaymentMade( msg.sender, thisRequest.recipient, thisRequest.value );
+    }
+    
+}
 
 
 contract ProjectHub {
